25a05e863444cfdbc3e757386cc3aafa08f3fac1
Emmazzzing <liu.jing6@northeastern.edu>
2024-05-01 08:39:09 -0700

    header and sidedrawer

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./MainNavigation.css";
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const MainNavigation = (props) => {
+  const [drawerIsOpen, setdrawerIsOpen] = useState(false);
+  const OpenDrawer = () => {
+    setdrawerIsOpen(true);
+  };
+
+  const CloseDrawer = () => {
+    setdrawerIsOpen(false);
+  };
+  return (
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={CloseDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={OpenDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlace</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+};
+
+export default MainNavigation;
